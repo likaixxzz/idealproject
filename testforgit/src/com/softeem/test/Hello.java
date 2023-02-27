890694c698c083f597aaff42f46ee679c42ecc16
likai <1487986775@qq.com>
2023-02-27 16:23:52 +0800

    我的项目第三次提交

--- testforgit/src/com/softeem/test/Hello.java
+++ testforgit/src/com/softeem/test/Hello.java
@@ -3,5 +3,7 @@ package com.softeem.test;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("我修改了一次代码");
+        System.out.println("我对代码进行了第二次修改");
     }
 }
